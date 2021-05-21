04d9656ea5348fff06d007fde50c4afab45f6c03
awenelo <56133692+awenelo@users.noreply.github.com>
2021-05-21 09:18:28 -0400

    Start adding support for puzzle start

--- application/blueprints/index.py
+++ application/blueprints/index.py
@@ -2,10 +2,15 @@ from flask import Blueprint, render_template, current_app
 
 bp = Blueprint("index", __name__, url_prefix="/")
 
+def get_start_page():
+    return 0
+
 @bp.route("/")
 def home():
     return render_template("index.html")
 
 @bp.route("/game/<pageid>")
-def param(param):
+def param(pageid):
+    if pageid.lower=="start":
+        pageid = get_start_page()
     return render_template("gamepage.html", param=param)
